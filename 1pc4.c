0a52883441088c768b45a6bf91a0019b7310efdf
Yodeeha <146912446+Yodeeha@users.noreply.github.com>
2023-10-08 22:13:59 +0700

    1pc4.c

---
+++ 1pc4.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int main(){
+    printf("Hello, ini adalah Data saya\n");
+    printf("Nama saya %s\n", "Yosafat Deguis Hutabarat");
+    printf("Usia saya %d\n",18);
+    return 0;
+}
