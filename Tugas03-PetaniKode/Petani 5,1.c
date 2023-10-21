666158476c0c904aa4d3bdde553ecab7f6bb275d
Yosafat Deguis Hutabarat <itsjunior54@gmail.com>
2023-10-21 17:23:35 +0700

    perbaikan nama folder

---
+++ Tugas03-PetaniKode/Petani 5,1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+     int berat;
+     int tinggi =189;
+
+     berat = 78;
+     printf("Berat badan saya adalah %ikg dan tinggi saya adalah %icm", berat,tinggi );
+     
+     return 0;
+}
---
+++ Tugas03-PetaniKode/Petani 5,2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int usia = 19;
+    float berat = 65.4;
+    double tinggi = 164.3;
+    char jenis_kelamin = 'L';
+
+    printf("Usia: %i tahun \n",usia);
+    printf("Berat : %.2f kg\n",berat);
+    printf("Tinggi : %.2fcm\n", tinggi);
+    printf("Jenis kelamin : %c\n", jenis_kelamin);
+    
+    return 0;
+}
