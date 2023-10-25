12bcad4abb2018d197c03b45b45bd5a8d2a95d54
Yosafat Deguis Hutabarat <itsjunior54@gmail.com>
2023-10-25 10:07:15 +0700

    cleaning up

--- 1pc4.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> 
-int main(){
-    printf("Hello, ini adalah Data saya\n");
-    printf("Nama saya %s\n", "Yosafat Deguis Hutabarat");
-    printf("Usia saya %d\n",18);
-    return 0;
-}
--- 2pc4.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    puts("Hello World");
-    puts("Nama saya Yosafat Deguis");
-    return 0;
-
-}
